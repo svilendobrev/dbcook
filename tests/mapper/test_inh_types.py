e9768d4d31dd015d48ff4bbdf565530e09221b68
svilen_dobrev <svilen_dobrev@1c027a48-7a35-0410-bfb4-ab0b45e9e153>
2007-07-20 17:21:05 +0000

    config/picture

--- tests/mapper/test_inh_types.py
+++ tests/mapper/test_inh_types.py
@@ -4,15 +4,15 @@ from tests.util import context
 from test4mapper import gen_inh_types, inh_symb, MapperCase
 
 def picture( inh):
-    nl = '\n'
-    s = 10*'/'+ 'Trying ... ' + 10*'\\'+nl
     t = ' '
-    s += 9*t + 'A' + nl
-    s += 7*t + inh_symb( inh[0], 'l') + t + inh_symb( inh[1], 'r') + nl
-    s += 6*t + 'B' + 4*t + 'D'+nl
-    s += 4*t + inh_symb( inh[2], 'l') + t + inh_symb( inh[3], 'r') + nl
-    s += 3*t + 'C' + 4*t + 'E'+nl
-    return s
+    return '\n'.join( [
+        10*'/'+ 'Trying ... ' + 10*'\\',
+        9*t + 'A',
+        7*t + inh_symb( inh[0], 'l') + t + inh_symb( inh[1], 'r'),
+        6*t + 'B' + 4*t + 'D',
+        4*t + inh_symb( inh[2], 'l') + t + inh_symb( inh[3], 'r'),
+        3*t + 'C' + 4*t + 'E',
+    ])
 
 
 class InhCaseParams( object):
@@ -85,9 +85,9 @@ from tests.util.case2unittest import get_test_suite
 testsuite = get_test_suite( param_gen( context), InhCase)
 
 if __name__ == '__main__':
+    context.SAdb.config.getopt()
     import unittest
     unittest.TextTestRunner( unittest.sys.stdout,
             verbosity=2, descriptions=True, ).run( testsuite)
-
 
 # vim:ts=4:sw=4:expandtab
