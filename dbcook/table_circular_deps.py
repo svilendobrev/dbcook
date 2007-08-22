08a8368f82ee1fa0e25f62c79da30a549d96e6b7
svilen_dobrev <svilen_dobrev@1c027a48-7a35-0410-bfb4-ab0b45e9e153>
2007-08-22 09:25:31 +0000

    finding-sqlutil-order

--- dbcook/table_circular_deps.py
+++ dbcook/table_circular_deps.py
@@ -7,7 +7,7 @@ sql_util = find_valid_fullname_import( '''
     sqlalchemy.sql_util
     sqlalchemy.sql.util
 ''')#this order!
-print sql_util
+
 def fix_table_circular_deps( tablelist, dbg =0, **kargs):
     if dbg: print 'fix_table_circular_dependencies...'
 
