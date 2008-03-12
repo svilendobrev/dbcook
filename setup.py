072ff0cdd24f5d9dc055bab1797a09fbbe620fb3
svilen_dobrev <svilen_dobrev@1c027a48-7a35-0410-bfb4-ab0b45e9e153>
2008-03-12 08:09:24 +0000

    fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name= 'dbcook',
-    version= '0.1',
+    version= '0.2',
     description= 'framework to abstract database-mapping of objects/relations as python declarations, completely hiding the DB where possible',
     author= 'svilen dobrev',
     author_email= 'svilen_dobrev@users.sourceforge.net',
@@ -18,7 +18,7 @@ setup(
     license = "MIT License",
 
     classifiers='''
-Development Status :: 3 - Alpha
+Development Status :: 4 - Beta
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
@@ -37,6 +37,10 @@ Currently the available builder is over SQLAlchemy as backend.
 
 repository: svn co https://dbcook.svn.sf.net/svnroot/dbcook/trunk
 
+Additions:
+ * complete bitemporal class-mix-in : trunk/dbcook/misc/timed2/
+ * (independent) automatic aggregating columns : trunk/dbcook/misc/aggregator*
+
 Usage cases/levels:
  * DB-definition - completely hides/automates the table/ column/ key/ constraint/
     mapper/ whatever creation. The user can control certain characteristics of the way
@@ -50,32 +54,30 @@ Usage cases/levels:
     python functions/expressions (over objects' attributes) into backend/SQL clauses:
     ``lambda self: (self.friend.manager.age < 40) & self.name.endswith('a')``
  * writing own reflectors (that walk the declared classes and
-    extracts info from them), eventualy allowing different "language/syntax"
+    extract info from them), eventualy allowing different "language/syntax"
 
 Features:
  * data column types - the actual mapping is separate from object declaration
  * reference columns - plain, forward-declared, self-referential -> foreign keys
- * automatic solving of cyclical references/ dependencies
-    (putting proper alter_table / post_update)
- * class inheritance, class inclusion (inheritance without
-    database mapping of the base), and virtual classes (those never
-    have instances). More in mapcontext._Base
- * polymorphism - giving 3 kinds of queries for each mapped class:
-    all, base-only, subclasses only
- * any combination of table-inheritance-types within the tree
-    (concrete/joined/no-single-yet) - defined localy or hierarchicaly;
+ * automatic solving of cyclical references/ dependencies (proper alter_table / post_update)
+ * class inheritance, class inclusion (inheritance without db-mapping of the base),
+    and virtual classes (without instances). More in mapcontext._Base
+ * polymorphism - 3 kinds of queries for each mapped class: all, base-only, subclasses only
+ * any combination of table-inheritance-types within the tree -
+    joined/ concrete/ no-single-yet - defined localy or hierarchicaly;
     beware that sql-polymorphism only works with joined-table for now
  * associations (many-to-many) - implicit and explicit
- * dbcook.usage.samanager is a correct context-like keeper of all db-related SQLAlchemy
-    things in one place, which on destroy() will try hard to clear _all_ side-effects of its
-    existence, allowing reuse of same objects within several/different subsequent DB-mappings.
+ * collections  (one-to-many)
+ * dbcook.usage.samanager is a correct context-like keeper of all SQLAlchemy
+    things in one place; on destroy() will try hard to clear _all_ side-effects of its
+    existence, allowing reuse of same objects in several/different subsequent DB-mappings.
 
 To use the framework, the internal dbcook/ has to be accessible somehow (PYTHONPATH or else).
 Dependencies:
  * kjbuckets (from gadfly) for graph-arithmetics
- * SQLAlchemy, 0.3.6+?
+ * SQLAlchemy, both 0.3 and 0.4
 
-Example:
+Example (trunk/dbcook/usage/example/):
 ``
 import dbcook.usage.plainwrap as o2r
 class Text( o2r.Type): pass
