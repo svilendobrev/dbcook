7c89dc71425234a9c5f73d427603a731965679ac
svilen_dobrev <svilen_dobrev@1c027a48-7a35-0410-bfb4-ab0b45e9e153>
2008-11-30 16:11:57 +0000

    move-here

---
+++ dbcook/usage/orr.py
@@ -0,0 +1,20 @@
+#$Id$
+# -*- coding: cp1251 -*-
+
+import sqlalchemy
+def or2union( qbase, filters4subqueries, extra_filter_per_query =None):
+    uu = []
+    for a in filters4subqueries:
+        if isinstance( a, tuple): a=a[0]    #HACK for (expr,name)
+        ra = qbase.filter( a)
+        if extra_filter_per_query is not None:
+            ra = ra.filter( extra_filter_per_query )
+        if len(filters4subqueries)<=1:
+            return ra
+        s = ra._compile_context().statement
+        s.use_labels = True
+        uu.append( s)
+    filt = sqlalchemy.union( *uu)
+    return qbase.select_from( filt)
+
+# vim:ts=4:sw=4:expandtab
