c35ba5e14c1c406fb6101d5b0ec6dde7b4c49db4
svilen.dobrev <www.svilendobrev.com>
2012-07-19 18:48:34 +1000

    del

--- dbcook/reflector.py
+++ dbcook/reflector.py
@@ -17,7 +17,7 @@ class Reflector:
             me.klas = item_klas
             #assert cardinality in me._cardinality
             #me.cardinality = cardinality
-            from util.attr import setattr_from_kargs
+            from dbcook.util.attr import setattr_from_kargs
             setattr_from_kargs( me, kargs,
                 as_value= False,    #bool,
                 lazy    = 'default' or bool or None,
